155a2e7b6c12728aa6ad707b86e12a3e333f162f
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-04-18 17:02:02 +1200

    turn off page delay for more violence

--- options.rb
+++ options.rb
@@ -4,4 +4,4 @@ $options = OpenStruct.new
 
 $options.datastore = 1
 $options.global_prefix = 'audit' # changing this will orphan hundreds of redis keys.
-$options.page_delay = 1
+$options.page_delay = 0
