08ea43c906b9f73d9c54d76e161479816017b714
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-05-01 12:29:12 +1200

    added solr for options

--- options.rb
+++ options.rb
@@ -5,3 +5,4 @@ $options = OpenStruct.new
 $options.datastore = 1
 $options.global_prefix = 'audit' # changing this will orphan hundreds of redis keys.
 $options.page_delay = 0.1
+$options.solr = 'http://search.tki.org.nz:8983/solr'
