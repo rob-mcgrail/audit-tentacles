7991256b1b91e3e63f66f505f9575bec0941c8d9
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-05-02 14:43:23 +1200

    includes for new class

--- lib/audit_tentacles.rb
+++ lib/audit_tentacles.rb
@@ -7,9 +7,10 @@ $term = HighLine.new
 
 require './options'
 require './lib/audit_tentacles/redis'
+require './lib/audit_tentacles/mms'
+require './lib/audit_tentacles/ezpub'
 require './lib/audit_tentacles/sso_auth'
 require './lib/audit_tentacles/media'
 require './lib/audit_tentacles/slurp'
 require './lib/audit_tentacles/crawler'
-require './lib/audit_tentacles/mms'
 require './lib/audit_tentacles/output'
